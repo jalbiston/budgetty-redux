eff6938fd3b9f1a2f74749f23202b1985c81d040
jalbiston <jacob.albiston@gmail.com>
2021-11-09 11:28:37 -0700

    rest of the thing, study it

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 import budgetReducer from './budgetReducer.js'
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
-    budget: budgetReducer
+    budget: budgetReducer,
+    user: userReducer
 })
 
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
